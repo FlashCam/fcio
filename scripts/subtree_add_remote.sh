99f2316dde5d60c00cec97c7fd660aef7d535dcd
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2022-05-19 13:11:21 +0200

    Added scripts directory: shell scripts to use and document git subtree environment.

---
+++ scripts/subtree_add_remote.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Adding bufio origin as remote 'bufio'"
+git remote add bufio https://git.mpi-hd.mpg.de/fwerner/bufio.git
+echo "Adding tmio origin as remote 'tmio'"
+git remote add tmio https://git.mpi-hd.mpg.de/fwerner/tmio.git
