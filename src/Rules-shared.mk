e18a595a5f171f2b0959f00efc68caea749de538
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2023-11-17 17:13:26 +0100

    Add meson build.

--- src/Rules-shared.mk
+++
@@ -1,6 +0,0 @@
-TARGET      := lib/libfcio.so
-SOURCES     := fcio.c time_utils.c
-SRC_INCDIRS := ../externals/tmio/src
-TGT_LDFLAGS := -shared -Llib
-TGT_LDLIBS  := -ltmio -lbufio
-TGT_PREREQS := lib/libfcio.a
